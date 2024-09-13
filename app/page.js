b357e94a284f9ba452916609d9c5b5c40347f6d5
LuigiR1 <luigi.rossi@edu.sait.ca>
2024-09-13 14:25:51 -0600

    Week-2 Assignment
    I finished the week-2 assignment and commiting/pushing to github

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
+      <h1>CPRG 306: Web Developement 2 - Assignments</h1>
+      <Link href="./week-2/" className="underline text text-cyan-600 hover:text-cyan-400">Week-2</Link>
 
     </main>
     
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <main>
+            <h1>Luigi Rossi</h1>
+            <Link href="https://github.com/LuigiR1/cprg306-assignments">Github Link</Link>
+        </main>
+    );
+}
