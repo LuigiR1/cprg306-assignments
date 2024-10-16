3cfd6d6780ca04a4340eca5e4449b5dbe93f7425
LuigiR1 <luigi.rossi@edu.sait.ca>
2024-10-15 21:10:08 -0600

    Week-5
    Finished week-5 assignment

---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import NewItem from './new-item';
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <NewItem />
+    </div>
+  );
+}
