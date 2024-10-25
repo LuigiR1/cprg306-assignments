26d1022d55ff08010700a26520e66f3cc6b174bc
LuigiR1 <luigi.rossi@edu.sait.ca>
2024-10-25 14:27:33 -0600

    week-7 Assignment
    Finished week-7 assignment

---
+++ app/week-7/item.js
@@ -0,0 +1,10 @@
+export default function Item({name, quantity, category}) {
+
+    return (
+        <li className="bg-orange-300 mb-3 flex flex-col justify-between p-4 border-l border-t border-4 border-orange-600">
+            <div className="font-semibold text-lg text-gray-900">{name}</div>
+            <div className="text-gray-600">Buy {quantity} in {category}</div>
+        </li>
+
+    );
+}
---
+++ app/week-7/page.js
@@ -0,0 +1,20 @@
+"use client";
+import React, { useState } from 'react';
+import NewItem from './new-item';
+import ItemList from './item-list';
+import itemsData from './items.json';
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  return (
+    <main>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
+    </main>
+  );
+}
